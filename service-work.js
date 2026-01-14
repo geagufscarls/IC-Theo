5f18631515638e1cb6f88aee73573bb9c420f46d
geagufscarls <geagufscarls@gmail.com>
2026-01-14 20:58:35 -0300

    Update service-work.js

--- service-work.js
+++ service-work.js
@@ -1,19 +1,33 @@
-const CACHE_NAME = "pragas-offline-v1";
+const CACHE_NAME = "pragas-offline-v3";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./service-worker.js",
-  "https://cdn.jsdelivr.net/npm/xlsx/dist/xlsx.full.min.js"
+  "./xlsx.full.min.js"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
@@ -23,4 +37,3 @@ self.addEventListener("fetch", event => {
     })
   );
 });
-
